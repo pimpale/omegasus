db3460c5beaa19769def87d99afdbb40dcc77d6a
Garni Gharibian <gharibian.garni0@gmail.com>
2023-03-20 00:05:47 -0700

    Add training transformer script.

---
+++ amogus-trans/transformer/train.py
@@ -0,0 +1,40 @@
+import os
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from osus_dataset import OSUSDataset
+from util import get_csv_data, train, generate, generate_conversation, generate_conversation_base_model
+import torch
+
+DATA_DIR = '../data_dir'
+MODEL_DIR = 'model_dir'
+SAVED_MODEL_PREFIX = 'bigboi'
+    
+def main():
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    
+    train_data = get_csv_data(os.path.join(DATA_DIR, 'train'))
+    
+    train_dataset = OSUSDataset(train_data, tokenizer, 1024) # OSUSDataset Obj
+    
+    model = train(train_dataset, 
+                  model, 
+                  batch_size=2, 
+                  epochs=20,
+                  lr=2e-5,
+                  output_dir=MODEL_DIR,
+                  output_prefix=SAVED_MODEL_PREFIX,
+                  save_model_on_epoch=True,
+                  save_best_model=True
+                  )
+    
+    # print(train_data[10])
+
+    # generated_message = generate(model, tokenizer, "Red\tcrewmate\t", entry_count=10, entry_length=100)
+
+if __name__ == '__main__':
+    # main()
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    model.load_state_dict(torch.load('model_dir/bigboi-best-2.pt'))
+    
+    print(generate_conversation(model, tokenizer, 'Red', 'Green', 'Blue'))
